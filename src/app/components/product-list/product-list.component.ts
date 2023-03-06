660607e4914e14fb897abca89783bb3f46cff9a6
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-03-06 04:57:46 +0100

    Fetch products from data.json & display them in view

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.getProduct().subscribe(data => {
+      // add quantity to each product
+      for (let i = 0; i < data.length; i++) {
+        const product = data[i];
+        product['quantity'] = 1;
+      }
+
+      this.products = data;
+    })
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,10 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Product } from '../model/product';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class ProductService implements OnInit {
 
-  constructor() { }
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void { }
+
+  getProduct(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>('assets/data.json');
+  }
 }
