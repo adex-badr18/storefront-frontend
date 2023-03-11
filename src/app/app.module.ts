ca2918baf014d32cbb8591579b5d039f41729f1b
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-03-11 06:21:59 +0100

    Configure routing and add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartComponent } from './components/cart/cart.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AddToCartComponent } from './components/add-to-cart/add-to-cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    NavBarComponent,
+    ProductItemComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    AddToCartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
