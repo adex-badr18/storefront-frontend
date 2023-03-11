c4c97e7fe6812925c7c7b5d76eb860a4a4232555
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-03-11 18:41:45 +0100

    Code logic for product details view

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { CartServiceService } from 'src/app/services/cart-service.service';
+import { ProductInfoService } from 'src/app/services/product-info.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Product[] = [];
+
+  constructor(
+    private cartService: CartServiceService,
+    private productInfoService: ProductInfoService) { }
+
+  ngOnInit(): void {
+    this.product = this.productInfoService.showInfo();
+  }
+
+  clearInfo() {
+    this.productInfoService.clearInfo();
+    this.product = [];
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    alert(`${product.name} added to cart.`);
+  }
 
 }
