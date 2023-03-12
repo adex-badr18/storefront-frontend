5d346ba786c5d7ae14986e1b77a8c0cda7162804
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-03-12 13:50:48 +0100

    Add CSS to product-list view

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -8,6 +8,7 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  title: string = 'Top deals';
   products: Product[] = [];
 
   constructor(private productService: ProductService) { }
